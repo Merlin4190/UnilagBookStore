c5c9c125f6c0033cde7fce634d15c12915bc1788
merlin4190 <abideklove@gmail.com>
2023-05-14 20:21:05 +0100

    Modelling completed

--- UnilagBookstore.Api/Program.cs
+++ UnilagBookstore.Api/Program.cs
@@ -1,6 +1,10 @@
+using UnilagBookstore.Infrastructure.Context;
+using UnilagBookstore.Services.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDatabaseService<AppDbContext>(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
